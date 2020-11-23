8835b82b3cd31a7757b3d42a55d80cdccf4caba2
sutehuna <sutehuna@gmail.com>
2020-11-23 18:13:54 +0900

    必要だと思われる列をUsersに追加

---
+++ db/migrate/20201123091216_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :postal_code, :integer
+    add_column :users, :address, :string
+    add_column :users, :self_introduction, :text
+  end
+end
