04249db936064ec5a765d742c5b200ae317bff2c
sutehuna <sutehuna@gmail.com>
2020-11-29 11:46:36 +0900

    指摘事項の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :profiles, only: %i[index show edit update]
+  resources :profiles, only: %i[index show]
   devise_for :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
