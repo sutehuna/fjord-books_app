8f50bf8c5da6277be94e24677fa7b4fe4d345944
sutehuna <sutehuna@gmail.com>
2020-12-01 18:36:41 +0900

    deviseのストロングパラメーターの追加とuserコントローラからedit,updateアクションの除去

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,26 +5,7 @@ class ProfilesController < ApplicationController
     @users = User.order(:id).page(params[:page])
   end
 
-  def edit
-    @user = current_user
-  end
-
   def show
     @user = User.find(params[:id])
-  end
-
-  def update
-    @user = current_user
-    if @user.update(profile_params)
-      redirect_to controller: :profiles, action: :show
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def profile_params
-    params.require(:user).permit(:postal_code, :address, :self_introduction)
   end
 end
