f68c098ac306cddf0028c092740a984e54ca3883
sutehuna <sutehuna@gmail.com>
2020-12-05 15:36:08 +0900

    コントローラのフォーマットによる分岐を削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -26,29 +26,20 @@ class ReportsController < ApplicationController
   def create
     params[:report][:user_id] = current_user.id
     @report = Report.new(report_params)
-
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to @report, notice: 'Report was successfully created.' }
-        format.json { render :show, status: :created, location: @report }
-      else
-        format.html { render :new }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to @report, notice: 'Report was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /reports/1
   # PATCH/PUT /reports/1.json
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to @report, notice: 'Report was successfully updated.' }
-        format.json { render :show, status: :ok, location: @report }
-      else
-        format.html { render :edit }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.update(report_params)
+      redirect_to @report, notice: 'Report was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -56,10 +47,7 @@ class ReportsController < ApplicationController
   # DELETE /reports/1.json
   def destroy
     @report.destroy
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: 'Report was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to reports_url, notice: 'Report was successfully destroyed.'
   end
 
   private
