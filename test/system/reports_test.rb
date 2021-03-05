e50c63b88fb30bc558491c93454d5eeec52e860b
sutehuna <sutehuna@gmail.com>
2021-03-05 23:45:04 +0900

    レポートを作成するシステムテストを追加

---
+++ test/system/reports_test.rb
@@ -0,0 +1,24 @@
+require "application_system_test_case"
+
+class ReportsTest < ApplicationSystemTestCase
+
+  setup do
+    visit root_url
+    fill_in 'Eメール', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
+  end
+
+  test 'creating a Report' do
+    visit reports_path
+    click_on '日報'
+    click_on '新規作成'
+    fill_in 'タイトル', with: 'タイトル1'
+    fill_in '内容', with: '内容1'
+    click_on '登録する'
+
+    assert_text '日報が作成されました。'
+    assert_text 'タイトル1'
+    assert_text '内容1'
+  end
+end
