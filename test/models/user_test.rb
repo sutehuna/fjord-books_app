536cd285d1d00822bb0667028f5f4078d259cdcd
sutehuna <sutehuna@gmail.com>
2021-03-08 17:12:28 +0900

    不要な空行を除去, userの作成をsetupで行うようにした

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,46 +3,38 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test '#following?' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
+  def setup
+    @me = User.create!(email: 'me@example.com', password: 'password')
+    @she = User.create!(email: 'she@example.com', password: 'password')
+  end
 
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
+  test '#following?' do
+    assert_not @me.following?(@she)
+    @me.follow(@she)
+    assert @me.following?(@she)
   end
 
   test '#followed_by' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-
-    assert_not she.followed_by?(me)
-    me.follow(she)
-    assert she.followed_by?(me)
+    assert_not @she.followed_by?(@me)
+    @me.follow(@she)
+    assert @she.followed_by?(@me)
   end
 
   test '#follow' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
+    assert_not @me.following?(@she)
+    @me.follow(@she)
+    assert @me.following?(@she)
   end
 
   test '#unfollow' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
-    me.unfollow(she)
-    assert_not me.following?(she)
+    assert_not @me.following?(@she)
+    @me.follow(@she)
+    assert @me.following?(@she)
+    @me.unfollow(@she)
+    assert_not @me.following?(@she)
   end
 
   test '#name_or_email' do
-    user = User.new(email: 'hoge@example.com')
-    assert_equal 'hoge@example.com', user.name_or_email
+    assert_equal 'me@example.com', @me.name_or_email
   end
 end
